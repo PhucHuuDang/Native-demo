5aa296e65aa95115921dc69864f2cf15a6238bc3
HarryDang <danghuuphuc001@gmail.com>
2024-08-20 23:24:11 +0700

    feat(tab-screen): docks items for the navigation

--- app/index.tsx
+++ app/index.tsx
@@ -7,9 +7,10 @@ import { StatusBar } from "expo-status-bar";
 const App = () => {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-4xl font-bold">Harry Dang</Text>
-      <Link href="/profile" style={{ color: "blue" }} className="text-4xl">
-        Profile
+      <Text className="text-4xl font-bold font-pblack">Harry Dang</Text>
+      <StatusBar style="auto" />
+      <Link href="/home" style={{ color: "blue" }} className="text-4xl">
+        Home
       </Link>
     </View>
   );
